ab90680c915345429b2f934c4b5370339e36918e
lkinnear <s0933963@sms.ed.ac.uk>
2018-09-20 15:20:55 +0100

    v minor changes to colorbar code

--- cosmo_production_scripts/CRN_production_from_output.py
+++ cosmo_production_scripts/CRN_production_from_output.py
@@ -8,7 +8,7 @@ Created on Thu Aug 16 16:04:44 2018
 import matplotlib.pyplot as plt
 import numpy as np
 
-data2 = np.genfromtxt('C:/Workspace/github/LSDMixingModel/Runs/pomd/p_trans_out.pout', delimiter=' ',skip_header=0, names=['time', 'bn', 'pid','z_loc','s_loc','d_loc','buff','page','osl','be_conc','c_conc','ne_conc'])
+data2 = np.genfromtxt('C:/Workspace/github/LSDMixingModel/Runs/flux_test/p_trans_out.pout', delimiter=' ',skip_header=0, names=['time', 'bn', 'pid','z_loc','s_loc','d_loc','buff','page','osl','be_conc','c_conc','ne_conc'])
 be_conc =data2['be_conc'] 
 #Depth   
 d_loc =data2['d_loc']
@@ -26,10 +26,10 @@ plt.tick_params(axis='both', which='major', labelsize=8)
 cb = ax.scatter(s_loc, z_loc,c=be_conc, cmap=plt.cm.YlOrBr, s= 10, label = 'Be Concentration', lw = 0)
 ax.set_xlabel("Distance Downslope (m)")
 ax.set_ylabel("Elevation (m)")
-#ax.set_xlim(0,58)
-#ax.set_ylim(60,0)
+#ax.set_xlim(10,20)
+#ax.set_ylim(112,114)
 axcb = plt.colorbar(cb)
-axcb.set_clim(0,20000)
+cb.set_clim(vmin=0,vmax=20000)
 #axcb.ax.tick_params(labelsize=1)
 axcb.set_label('$^{10}$Be Concentration', size=15)
 #plt.savefig('test_hillslope_crn_conc_mix_0.1.png', dpi=800, bbox_inches='tight')
